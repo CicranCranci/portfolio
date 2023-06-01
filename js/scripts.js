abdb1e43871b962de18fde7b437117ac09b82277
CicranCranci <98583137+CicranCranci@users.noreply.github.com>
2023-06-01 20:47:03 +0300

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,77 @@
+
+
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Navbar shrink function
+    var navbarShrink = function () {
+        const navbarCollapsible = document.body.querySelector('#mainNav');
+        if (!navbarCollapsible) {
+            return;
+        }
+        if (window.scrollY === 0) {
+            navbarCollapsible.classList.remove('navbar-shrink')
+        } else {
+            navbarCollapsible.classList.add('navbar-shrink')
+        }
+
+    };
+
+    // Shrink the navbar 
+    navbarShrink();
+
+    // Shrink the navbar when page is scrolled
+    document.addEventListener('scroll', navbarShrink);
+
+    // Activate Bootstrap scrollspy on the main nav element
+    const mainNav = document.body.querySelector('#mainNav');
+    if (mainNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#mainNav',
+            rootMargin: '0px 0px -40%',
+        });
+    };
+
+    // Collapse responsive navbar when toggler is visible
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
+            }
+        });
+    });
+
+});
+
+function viewCV() {
+    var cvContainer = document.getElementById('cvContainer');
+    var pdfViewer = document.getElementById('pdfViewer');
+    var pdfURL = 'assets/img/portfolio/Cristian-Andrei_Ciuparu_CV2023mai20.pdf';
+
+    cvContainer.style.display = 'block';
+    pdfViewer.src = pdfURL;
+}
+// scripts.js
+
+// Function to handle form submission
+function handleFormSubmission(event) {
+    event.preventDefault(); // Prevent form submission
+
+    // Get form field values
+    var name = document.getElementById('name').value;
+    var email = document.getElementById('email').value;
+    var phone = document.getElementById('phone').value;
+    var message = document.getElementById('message').value;
+
+    // Do something with the form data (e.g., send it to a server)
+
+    // Reset the form
+    this.reset();
+}
+
+
+
+
